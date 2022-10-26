80aaf8ee7f8a389fe0cdc5c8da225f14ec76c9a5
ThorSilverRanger <amyskovets@gmail.com>
2022-10-27 00:32:08 +0300

    well done

---
+++ 26.10.22/js/hw3_myskovets.js
@@ -0,0 +1,8 @@
+var button = document.querySelector('button');
+
+button.onclick = function() {
+  var name = prompt('First part');
+  var name2 = prompt('Second part');
+  var name3 = prompt('Thirt part');
+  alert('We had: ' + name + name2 + name3);
+}
