1dd7675bf89c3fd9712c29f09c63b03300b0fc11
ThorSilverRanger <amyskovets@gmail.com>
2022-10-26 21:01:58 +0300

    well done

---
+++ 21.10.22/js/index.js
@@ -0,0 +1,8 @@
+const userName = prompt ('What is your name')
+alert('Hello ' + userName + '! ' + 'How are you?')
+const userMood = confirm ('Press OK if you happy now')
+if (userMood == true) {
+    alert( 'Im so glad!' );
+  } else {
+    alert( 'Im realy disapointed!' );
+  }
