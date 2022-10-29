56671579191caeae3b189fd11dd2136e35273b09
ThorSilverRanger <amyskovets@gmail.com>
2022-10-29 15:06:30 +0300

    well done

---
+++ 29.10.22/js/mathops.js
@@ -0,0 +1,9 @@
+const numberFirst = prompt('Input first number');
+const numberSecond = prompt('Input second number');
+
+console.log(numberFirst + numberSecond)
+console.log(numberFirst - numberSecond)
+console.log(numberFirst * numberSecond)
+console.log(numberFirst / numberSecond)
+console.log(numberFirst ** numberSecond)
+console.log(numberFirst % numberSecond)
