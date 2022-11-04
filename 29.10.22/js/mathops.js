6d5d6bc5b8e68485feb09989d09ae179413c8b87
ThorSR <110186611+ThorSilverRanger@users.noreply.github.com>
2022-11-04 09:59:31 +0200

    Delete 29.10.22 directory
    closed work

--- 29.10.22/js/mathops.js
+++
@@ -1,8 +0,0 @@
-const numberFirst = Number(prompt('Input first number'));
-const numberSecond = Number(prompt('Input second number'));
-console.log(numberFirst + numberSecond)
-console.log(numberFirst - numberSecond)
-console.log(numberFirst * numberSecond)
-console.log(numberFirst / numberSecond)
-console.log(numberFirst ** numberSecond)
-console.log(numberFirst % numberSecond)
